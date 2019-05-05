64a7cc2b4f72746b13b41bc3fcfb74af9344289c
Cristian Santos <csantos@nativapps.co>
2019-05-05 16:46:08 +0000

    Lección 12

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,24 @@
 import { GraphQLServer } from "graphql-yoga"
 
-/** Type definitions (Type Schema) */
+/** 
+ * Type definitions (Type Schema) 
+ * Scalar Types (Single value) => String, Boolean, Int, Float, ID 
+ * Non-Scalar => Objects and Arrays
+ */
+
 const typeDefs = `
     type Query {
-        hello: String!
+        id: ID!
         name: String!
-        location: String!
-        bio: String!
+        age: Int!
+        employed: Boolean!
+        GPA: Float
+        
+        title: String!
+        price: Float!
+        releaseYear: Int
+        rating: Float
+        inStock: Boolean!
     }
 `
 /** 
@@ -14,22 +26,19 @@ const typeDefs = `
  */
 const resolvers = {
     Query: {
-        hello () {
-            return "This is my first query!" 
-        },
+        id: () => "abc123",
         name: () => "Cristian Santos",
-        location: function () {
-            return "Barranquilla"
-        },
-        bio () {
-            return "Vivo en Barranquilla y trabajo en NativApps"
-        }
+        age: () => 24,
+        employed: () => true,
+        GPA: () => 4.65,
+
+        title: () => "Introduction to Artificial Intelligence",
+        price: () => 19.99,
+        releaseYear: () => 2018,
+        rating: () => 4.5,
+        inStock: () => true
     }
 }
 
-const server = new GraphQLServer({
-    typeDefs,
-    resolvers
-})
-
+const server = new GraphQLServer({ typeDefs, resolvers })
 server.start({ port: 3000 }, () => console.log('Server is up!'))
