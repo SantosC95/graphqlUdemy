2d531a9489081d0806ed9388075aed51dc309a21
Cristian Santos <csantos@nativapps.co>
2019-05-05 16:32:35 +0000

    Lección 11

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,35 @@
-import defaultSum, { doSum as mySum, doSubtract } from "./modules"
+import { GraphQLServer } from "graphql-yoga"
 
-const a = 10
-const b = 7
+/** Type definitions (Type Schema) */
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`
+/** 
+ * Resolvers
+ */
+const resolvers = {
+    Query: {
+        hello () {
+            return "This is my first query!" 
+        },
+        name: () => "Cristian Santos",
+        location: function () {
+            return "Barranquilla"
+        },
+        bio () {
+            return "Vivo en Barranquilla y trabajo en NativApps"
+        }
+    }
+}
 
-console.log('Suma por defecto', defaultSum(a, b))
-console.log('Suma', mySum(a, b))
-console.log('Resta', doSubtract(a, b))
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start({ port: 3000 }, () => console.log('Server is up!'))
--- app/src/modules.js
+++
@@ -1,5 +0,0 @@
-const doSum = (a, b) => a + b
-const doSubtract = (a, b) => a - b
-
-export default doSum
-export { doSum , doSubtract }
