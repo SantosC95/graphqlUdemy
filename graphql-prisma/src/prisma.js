cf09b6772d7f2bf1b663d1cd8df34c9ed22408dc
Cristian Santos <csantos@nativapps.co>
2019-08-14 13:39:09 +0000

    Variable de entorno para datos sensibles

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -4,7 +4,7 @@ import { fragmentReplacements } from "./resolvers"
 const prisma = new Prisma({
     typeDefs: "src/generated/prisma.graphql",
     endpoint: process.env.PRISMA_ENDPOINT,
-    secret: "Prism4S3rve3S3cr3t",
+    secret: process.env.PRISMA_SECRET,
     fragmentReplacements
 })
 
